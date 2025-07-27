75fdd30e058979521dd3370580957bb8460dc39d
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-07-27 22:49:43 +0500

    small changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function respondRemote() {
   document.getElementById("responseMessage").innerText =
-    "Thank you for the update. I truly appreciate the opportunity. However, I am currently seeking a remote position, so I would not be able to attend an on-site role in Okara. Please do keep me in mind for any remote opportunities in the future.";
+    "Thank you for the update. I truly appreciate the opportunity. However, I am currently seeking a remote position so I would not be able to attend an on-site role in Okara. Please do keep me in mind for any remote opportunities in the future.";
 }
 
 function respondIgnore() {
   document.getElementById("responseMessage").innerText =
-    "You chose to ignore this message.";
+    "You chose to ignore this message .";
 }
 
 // Chart.js bar chart
@@ -18,7 +18,7 @@ const jobChart = new Chart(ctx, {
       {
         label: "Applications",
         data: [12, 4, 3, 5],
-        backgroundColor: ["#0d6efd", "#20c997", "#ffc107", "#dc3545"],
+        backgroundColor: ["#0d6efd", "#20c997", "#ffc107", "#dc3545","#ddd599"],
       },
     ],
   },
